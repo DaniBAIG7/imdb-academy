88ce0f785be6d91ad1e586d57bbf12c56cbdd288
Daniel Barrientos Iglesias <UO270162@uniovi.es>
2022-05-13 09:47:07 +0200

    NO HEAP
    Have to upload this (unfinished quirky code) to run the code on some better computer.

--- src/main/java/co/empathy/academy/search/ImdbApplication.java
+++ src/main/java/co/empathy/academy/search/ImdbApplication.java
@@ -1,13 +1,23 @@
 package co.empathy.academy.search;
 
+import co.empathy.academy.search.util.tsv_merger.TSVMerger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ImdbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImdbApplication.class, args);
+		try {
+			TSVMerger.cleanAkas("src/main/resources/cleaning/title.basics.sorted.cleaned.tsv",
+					"src/main/resources/cleaning/title.akas.sorted.tsv",
+					"src/main/resources/cleaning/title.akas.sorted.cleaned.tsv");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
